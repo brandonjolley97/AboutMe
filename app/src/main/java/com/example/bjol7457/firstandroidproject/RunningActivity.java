e5ea80179dbf36bda80d62a0ad50cd56ba0d28d6
brandonjolley97 <bjol7457@740s-pl-131.local>
2015-09-29 09:59:05 -0600

    Tons of Android Dev Stuff!

---
+++ app/src/main/java/com/example/bjol7457/firstandroidproject/RunningActivity.java
@@ -0,0 +1,9 @@
+package com.example.bjol7457.firstandroidproject;
+
+import android.content.intent;
+
+/**
+ * Created by bjol7457 on 9/29/15.
+ */
+public class RunningActivity {
+}
---
+++ app/src/main/java/com/example/bjol7457/firstandroidproject/StupidMorningActivity.java
@@ -0,0 +1,102 @@
+package com.example.bjol7457.firstandroidproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
+import android.view.View;
+import android.content.Intent;
+
+
+public class StupidMorningActivity extends AppCompatActivity
+{
+
+    private Button colorChangeButton;
+    private TextView basicText;
+    private RelativeLayout background;
+    private Button swapScreenButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stupid_morning);
+
+        background = (RelativeLayout) findViewById(R.id.backgroundLayout);
+        basicText = (TextView) findViewById(R.id.sillyText);
+        colorChangeButton = (Button) findViewById(R.id.sillyButton);
+        swapScreenButton = (Button) findViewById(R.id.swapScreenButton);
+
+        setupListeners();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void changeBackgroundColor()
+    {
+        int redColor;
+        int greenColor;
+        int blueColor;
+
+        redColor = (int)
+    }
+
+    private void changeVisibility()
+    {
+        if(basicText.getVisibility() == View.VISIBLE)
+        {
+            basicText.setVisibility(View.GONE);
+        }
+        else
+        {
+            basicText.setVisibility(View.VISIBLE);
+        }
+
+        basicText.setVisibility(View.GONE);
+    }
+
+    private void setupListeners()
+    {
+        colorChangeButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View buttonView)
+            {
+                //This is where the action happens!
+                changeBackgroundColor();
+                ChangeVisibility();
+            }
+        });
+
+        swapScreenButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View buttonView)
+            {
+                Intent myIntent = new Intent(buttonView.getContext(), RunningActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+    }
+}
