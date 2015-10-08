60dd08656391463f9ad5b718becca7bba386968c
brandonjolley97 <bjol7457@740s-pl-131.local>
2015-10-08 09:27:14 -0600

    minor change

--- app/src/main/java/com/example/bjol7457/firstandroidproject/StupidMorningActivity.java
+++ app/src/main/java/com/example/bjol7457/firstandroidproject/StupidMorningActivity.java
@@ -22,7 +22,8 @@ public class StupidMorningActivity extends AppCompatActivity
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stupid_morning);
 
